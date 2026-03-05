f1375d32e90b9c2c705cabeef4a198606e80dbe3
Mael Guilbert Costes <mguilber@3E7.42angouleme.fr>
2026-03-05 18:11:56 +0100

    fdshj

--- gui.c
+++ gui.c
@@ -1,21 +1,36 @@
 #include "so_long.h"
 
-int32_t main(int argc, char **argv)
+#include "MacroLibX/includes/mlx.h"
+
+int main(void)
 {
-    t_map *map;
-    mlx_t* mlx;
+    mlx_context mlx = mlx_init();
 
-mlx = mlx_init(WIDTH, HEIGHT, "Test MLX42", true);
-if (!mlx)
-{
-    puts(mlx_strerror(mlx_errno));
-    return (EXIT_FAILURE);
+    mlx_window_create_info window_info = { 0 };
+    window_info.title = "Hello World!";
+    window_info.width = 400;
+    window_info.height = 400;
+    mlx_window win = mlx_new_window(mlx, &window_info);
+
+    mlx_image target = mlx_new_image(mlx, 100, 100);
+
+    mlx_window_create_info target_info = { 0 };
+    target_info.render_target = target;
+    mlx_window target_win = mlx_new_window(mlx, &target_info);
+
+    {
+        // All of this will be rendered in `target`
+        mlx_clear_window(mlx, target_win, (mlx_color){ .rgba = 0xC16868FF });
+        mlx_string_put(mlx, target_win, 10, 10, (mlx_color){ .rgba = 0xFF2066FF }, "text");
+        mlx_pixel_put(mlx, target_win, 20, 40, (mlx_color){ .rgba = 0xFF0000FF });
+    }
+
+    mlx_put_image_to_window(mlx, win, target, 10, 10); // Render the target to the real window
+
+    mlx_destroy_window(mlx, target_win);
+    mlx_destroy_image(mlx, target);
+
+    mlx_destroy_window(mlx, win);
+
+    mlx_destroy_context(mlx);
 }
-
-mlx_loop(mlx);
-
-mlx_terminate(mlx);
-return (EXIT_SUCCESS);
-    return (0);
-}
-
